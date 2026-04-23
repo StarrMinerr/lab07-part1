98c821fbf0f1c59f152a360e1d5fd8d361709220
106564353 <myles8338@gmail.com>
2026-04-24 09:08:52 +1000

    fixed

---
+++ myfirst.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang  ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using PHP Variables, arrays and operators</title>
+
+</head>
+<body>
+    <h1>PHP Variables, arrays and operators</h1>
+    <?php
+    $marks = array (85, 85, 95);
+    $marks[1] = 90;
+    $ave = ($marks[0] + $Marks[2])/3;
+    if($ave >= 50)
+        $status = "PASSED";
+    else
+        $status = "FAILED";
+    echo "<p>The average score is $ave. You $status.</p>";
